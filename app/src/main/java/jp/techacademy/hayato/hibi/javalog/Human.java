bfc103aa13cc538e95fe9f92f85132735e756240
hayato.H <catshayato@gmail.com>
2018-12-06 15:22:28 +0900

    @Ovarrideの追加

--- app/src/main/java/jp/techacademy/hayato/hibi/javalog/Human.java
+++ app/src/main/java/jp/techacademy/hayato/hibi/javalog/Human.java
@@ -15,6 +15,7 @@ class Human extends Animal implements Thinkable {
     public void say() {
         Log.d("javatest", "私の名前は" + this.name + "です。年は" + this.age + "歳です。");
     }
+    @Override
     public void think(){
         Log.d("javatest", "私は" + this.hobby + "について考える");
     }
